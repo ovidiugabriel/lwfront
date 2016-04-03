8eed7380cb6ee42414bf3bf477c13dce01fc329a
ovidiugabriel <ovidiugabriel@gmail.com>
2016-04-03 23:49:10 +0300

    Create coral.rkt

---
+++ coral.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+; Identity function (operator)
+(define (id x)
+  x
+  )
+
+; Lazy function definition operator
+;
+; This can be used only as unary operator
+;
+(define (:= f)
+  (lazy f)
+  )
+
+;
+; String evaluation operator
+;
+; This can be used only as unary operator
+;
+(define ($ p)
+  (force p)
+  )
+
+; https://reference.wolfram.com/language/ref/Composition.html
+(define @* compose)
+
+; https://reference.wolfram.com/language/ref/Map.html
+(define /@ map)
+
+;
+; https://reference.wolfram.com/language/ref/Apply.html
+;
+; Example: (@@ + '(1 2 3))
+; Result: 6
+;
+(define @@ apply)
