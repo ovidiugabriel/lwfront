5dca19da0ec5d77d120a68da2927af24264fc6be
ovidiugabriel <ovidiugabriel@gmail.com>
2016-04-04 17:38:50 +0300

    Create stdint.rkt

---
+++ stdint.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(provide stdint:int8)
+(define stdint:int8 "int8_t")
+
+(provide stdint:uint8)
+(define stdint:uint8 "uint8_t")
+
+(provide stdint:int16)
+(define stdint:int16 "int16_t")
+
+(provide stdint:uint16)
+(define stdint:uint16 "uint16_t")
+
+(provide stdint:int32)
+(define stdint:int32 "int32_t")
+
+(provide stdint:uint32)
+(define stdint:uint32 "uint32_t")
+
+(provide stdint:int64_t)
+(define stdint:int64_t "int64_t")
+
+(provide stdint:uint64_t)
+(define stdint:uint64_t "uint64_t")
