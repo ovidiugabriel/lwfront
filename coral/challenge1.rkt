9b277f9048c8e66628e03169b363e98290ebdc0e
Ovidiu <ovidiugabriel@gmail.com>
2019-08-02 17:02:20 +0300

    Update challenge1.rkt

--- coral/challenge1.rkt
+++ coral/challenge1.rkt
@@ -1,4 +1,17 @@
 
 ;; Map[#^2 &, a + b + c] => a^2 + b^2 + c^2
 
- 
+(define a 1)
+(define b 2)
+(define c 3)
+
+(define (lpow2 x) (list expt x 2))
+(define expr (list + 'a 'b 'c))
+
+(define F (car expr))
+(define L
+  (map lpow2 (cdr expr)) )
+
+(define-namespace-anchor anchor)
+(define ns (namespace-anchor->namespace anchor))
+(eval (cons F L) ns)
